0d7b66836e275743aa7f9caf618a876a926be772
breno bronzere <brenobronzere@gmail.com>
2026-06-11 16:25:20 -0300

    criação de paleta de cores, ajuste de cores e background

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,14 @@
-
-
+import Link from 'next/link';
 export default function Home() {
   return (
-    <div>
-    <section className="flex -col items-center justify-center h-screen gap-4">
-      <h1 className="text-4xl font-bold">sec1</h1>
-    </section>
-    <section className="flex flex-col items-center justify-center h-screen gap-4 bg-gray-100">
-      <h1 className="text-4xl font-bold">sec2</h1>
-    </section>
-    <section className="flex flex-col items-center justify-center h-screen gap-4">
-      <h1 className="text-4xl font-bold">sec3</h1>
-    </section>
-    <section className="flex flex-col items-center justify-center h-screen gap-4">
-      <h1 className="text-4xl font-bold">sec4</h1>
-    </section>
+    <div className="flex flex-col items-center justify-center h-screen gap-4 bg-background">
+        <img src="/background.png" alt="Background" className="absolute inset-0 w-full h-full object-cover blur-lg opacity-30" />
+        
+        <Link key="/home" href="/home" className="text-lg text-[#20659e] hover:text-[#0844a5] flex flex-col items-center gap-4">
+          <h1 className="text-4xl font-bold text-[#213c66]">Prazer, meu nome é Breno</h1>
+          <p className="text-xl text-[#355168]">Desenvolvedor Front-end</p>
+        </Link>
+          
     </div>
   );
 }
--- components/nav.tsx
+++ components/nav.tsx
@@ -29,19 +29,19 @@ function Navbar() {
   }, []);
 
   const navLinks = [
-    { href: '/Home', label: 'Home' },
-    { href: '/projetos', label: 'Projects' },
-    { href: '/sobre', label: 'About' },
-    { href: '/contact', label: 'Contact' },
+    { href: '/home', label: 'Home' },
+    { href: '/projetos', label: 'Projectos' },
+    { href: '/stacks', label: 'Stacks' },
+    { href: '/contato', label: 'Contato' },
   ];
   return (
-    <nav className={`fixed w-full shadow-md transition-transform duration-300 z-50 ${isVisible ? 'translate-y-0' : '-translate-y-full'}`}>
+    <nav className={`fixed w-full shadow-md transition-transform duration-300 z-50 border-b border-[#213c66]${isVisible ? 'translate-y-0' : '-translate-y-full'}`}>
 
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8 ">
         <div className="flex justify-between">
           
           <div className="flex items-center">
-            <Link href="/" className="text-2xl text-gray-800">
+            <Link href="/" className="text-2xl ">
               Breno Bronzere
             </Link>  
           </div>
@@ -49,11 +49,11 @@ function Navbar() {
           <div className="hidden md:flex items-center space-x-8">
             {navLinks.map((link) => (
               <Link
-                key={link.href}
+                key={link.label}
                 href={link.href}
-                className={`text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md ttext-md font-semibold transition-colors ${
+                className={` hover:text-[#8FA9BE] px-3 py-2 rounded-md text-md font-semibold transition-colors ${
                   pathname === link.href
-                    ? 'bg-gray-500 text-white'
+                    ? 'bg-[#213c66] text-white'
                     : ''
                 }`}
               >
@@ -63,7 +63,7 @@ function Navbar() {
             
           </div>
           <div className="md:hidden flex items-center ">
-              <button onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} className="inline-flex items-center justify-center p-2 rounded-md text-gray-600 hover:text-gray-900 hover:bg-gray-100 focus:outline-none focus:ring-2 focusLring-inset focus:ring-gray-300">
+              <button onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} className="inline-flex items-center justify-center p-2 rounded-md text-[#20659e] hover:text-[#0844a5] hover:bg-[#20659e] focus:outline-none focus:ring-2 focusLring-inset">
                 {!isMobileMenuOpen ? <span>☰</span> : <span>✕</span>}
               </button>
             </div>
@@ -71,7 +71,7 @@ function Navbar() {
           <div className={`md:giden transition-all duration-300 ease-in-out ${isMobileMenuOpen ? "max-h-64 opacity-100" : "max-h-0 opacity-0 overflow-hidden"}`}>
             {navLinks.map((link) => (
               <Link
-                key={link.href} href={link.href} onClick={() => setIsMobileMenuOpen(false)} className={`block px-3 py-2 rounded-md text-base font-medium ${pathname === link.href ? "text-blue-600 bg-blue-50" : "text-gray-600 hover:text-gray-900 hover:bg-gray-50"}`}>
+                key={link.label} href={link.href} onClick={() => setIsMobileMenuOpen(false)} className={`block px-3 py-2 rounded-md text-base font-medium ${pathname === link.href ? "text-white bg-[#20659e]" : "text-[#20659e] hover:text-[#0844a5] "}`}>
                   {link.label}
               </Link>
               
